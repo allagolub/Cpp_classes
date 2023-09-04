8748a09a4394844e0010bdfe93943b7d8494f559
golub alla <golub.as@phystech.edu>
2023-09-04 11:30:22 +0300

    add files

---
+++ shared_ptr/shared_ptr.h
@@ -0,0 +1,130 @@
+﻿#ifndef AR_H
+#define AR_H
+
+#include <algorithm>
+#include <iostream>
+
+template <class T>
+class SharedPtr {
+ private:
+  struct Block {
+    size_t shared_count = 0;
+    size_t weak_count = 0;
+    explicit Block(size_t first = 0, size_t second = 0) : shared_count(first), weak_count(second) {
+    }
+  };
+  T* data_ = nullptr;
+  Block* block_ = nullptr;
+
+ public:
+  SharedPtr(T* ptr) : data_(ptr) {  // NOLINT
+    if (ptr == nullptr) {
+      block_ = nullptr;
+    } else {
+      block_ = new Block(1, 0);
+    }
+  }
+  SharedPtr(const SharedPtr& other) : data_(other.data_), block_(other.block_) {
+    if (block_ != nullptr) {
+      ++(block_->shared_count);
+    }
+  }
+  SharedPtr& operator=(const SharedPtr& other) {
+    if (this == &other) {
+      return *this;
+    }
+    if (this != &other) {
+      if (block_ != nullptr) {
+        --(block_->shared_count);
+        if (block_->shared_count == 0) {
+          delete data_;
+          if (block_->weak_count == 0) {
+            delete block_;
+          }
+        }
+      }
+      data_ = other.data_;
+      block_ = other.block_;
+      if (block_ != nullptr) {
+        ++(block_->shared_count);
+      }
+    }
+    return *this;
+  }
+  SharedPtr(SharedPtr&& other) noexcept : SharedPtr() {
+    Swap(other);
+  }
+  SharedPtr& operator=(SharedPtr&& other) noexcept {
+    if (this == &other) {
+      return *this;
+    }
+    if (this != &other) {
+      if (block_ != nullptr) {
+        --(block_->shared_count);
+        if (block_->shared_count == 0) {
+          delete data_;
+          if (block_->weak_count == 0) {
+            delete block_;
+          }
+        }
+      }
+    }
+    data_ = other.data_;
+    block_ = other.block_;
+    other.block_ = nullptr;
+    other.data_ = nullptr;
+    return *this;
+  }
+  ~SharedPtr() {
+    if (block_ == nullptr) {
+      return;
+    }
+    --(block_->shared_count);
+    if (block_->shared_count == 0) {
+      delete data_;
+      if (block_->weak_count == 0) {
+        delete block_;
+      }
+    }
+  }
+  SharedPtr() = default;
+  T& operator*() const {
+    return *data_;
+  }
+  T* operator->() const {
+    return data_;
+  }
+  T* Get() const {
+    return data_;
+  }
+  void Reset(T* ptr = nullptr) {
+    if (block_ != nullptr) {
+      --(block_->shared_count);
+      if (block_->shared_count == 0) {
+        delete data_;
+        if (block_->weak_count == 0) {
+          delete block_;
+        }
+      }
+    }
+    data_ = ptr;
+    block_ = nullptr;
+    if (ptr != nullptr) {
+      block_ = new Block(1, 0);
+    }
+  }
+  void Swap(SharedPtr<T>& other) {
+    std::swap(data_, other.data_);
+    std::swap(block_, other.block_);
+  }
+  explicit operator bool() const {
+    return (data_ != nullptr);
+  }
+  size_t UseCount() const {
+    if (block_ == nullptr) {
+      return 0;
+    }
+    return block_->shared_count;
+  }
+};
+#endif
---
+++ unique_ptr/unique_ptr.h
@@ -0,0 +1,59 @@
+﻿#ifndef AR_H
+#define AR_H
+
+#include <algorithm>
+#include <iostream>
+
+template <class T>
+class UniquePtr {
+ private:
+  T* ptr_;
+
+ public:
+  explicit UniquePtr(T* ptr) : ptr_(ptr) {
+  }
+  UniquePtr(const UniquePtr& other) = delete;
+  UniquePtr& operator=(const UniquePtr& other) = delete;
+  UniquePtr(UniquePtr&& other) noexcept : ptr_(other.ptr_) {
+    other.ptr_ = nullptr;
+  }
+  UniquePtr& operator=(UniquePtr&& other) noexcept {
+    if (this != &other) {
+      delete ptr_;
+      ptr_ = other.ptr_;
+      other.ptr_ = nullptr;
+    }
+    return *this;
+  }
+  ~UniquePtr() {
+    delete ptr_;
+  }
+  UniquePtr() {
+    ptr_ = nullptr;
+  }
+  T& operator*() const {
+    return *ptr_;
+  }
+  T* operator->() const {
+    return ptr_;
+  }
+  T* Get() const {
+    return ptr_;
+  }
+  T* Release() {
+    T* old = ptr_;
+    ptr_ = nullptr;
+    return old;
+  }
+  void Reset(T* ptr = nullptr) {
+    delete ptr_;
+    ptr_ = ptr;
+  }
+  void Swap(UniquePtr& other) {
+    std::swap(ptr_, other.ptr_);
+  }
+  explicit operator bool() const {
+    return (ptr_ != nullptr);
+  }
+};
+#endif
